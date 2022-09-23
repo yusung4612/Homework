96559195a87ebf5b75e533af263dc3df795a3556
yusung4612 <107028312+yusung4612@users.noreply.github.com>
2022-09-23 17:06:55 +0900

    Update Homework01.java

--- Homework01.java
+++ Homework01.java
@@ -5,8 +5,8 @@ public class Homework01 {
         int var1=5;
         int var2=2;
         double var3=(double)var1/var2;
-        int var4=(int)(var3*var2); //var3(5/2 이므로 2가 출력됨) * var2(2)
-        System.out.println(var4); // 결론적으로 var 4가 출력됨
+        int var4=(int)(var3*var2);
+        System.out.println(var4);
 
     }
 
